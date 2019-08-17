cec8774f43a96f431cce4fe45fc64149d9dd4627
Yunoh <yunoh78@gmail.com>
2019-08-17 11:40:38 -0400

    Update InstagramWDC.js

--- InstagramWDC.js
+++ InstagramWDC.js
@@ -55,9 +55,43 @@
             "tunameltsmyheart", "loki", "maple.the.pup", "omame_munchkin", "tibbythecorgi", "toby_littledude",
             "iggyjoey", "hamlet_the_piggy", "realdiddykong", "lionelthehog", "nevillejacobs", "jackthecockatiel"];
 
-        for (var i = 0; i < profiles[i].length; i++){
+        for (var i = 0; i < profiles.length; i++){
+            getProfile(table, profiles[i], doneCallback);
+        }
+    }
+
+    function getProfile(table, profile, doneCallback){
+        $.getJSON("https://www.instagram.com/" + profile + "/?__a=1", function(resp) {
             var tableData = [];
+            // Iterate over the JSON object
+            for (var j = 0, len = resp.graphql.user.edge_owner_to_timeline_media.edges.length; j < len; j++) {
+                tableData.push({
+                    "graphql__user__biography": resp.graphql.user.biography,
+                    "graphql__user__edge_followed_by__count": resp.graphql.user.edge_followed_by.count,
+                    "graphql__user__full_name": resp.graphql.user.full_name,
+                    "graphql__user__is_business_account": resp.graphql.user.is_business_account,
+                    "graphql__user__is_verified": resp.graphql.user.is_verified,
+                    "graphql__user__username": resp.graphql.user.username,
+                    "graphql__user__edge_owner_to_timeline_media__count": resp.graphql.user.edge_owner_to_timeline_media.count,
+                    "graphql_user_edge_owner_to_timeline_media_edges_node_edge_media_to_comment_count": resp.graphql.user.edge_owner_to_timeline_media.edges[j].node.edge_media_to_comment.count,
+                    "graphql_user_edge_owner_to_timeline_media_edges_node_edge_liked_by_count": resp.graphql.user.edge_owner_to_timeline_media.edges[j].node.edge_liked_by.count
+                });
+            }    
+            table.appendRows(tableData);
+            doneCallback(); 
+        });
+    }
+
+    /*
+    myConnector.getData = function(table, doneCallback){
+        var profiles = ["dorito.goldenboy", "bonebone29", "jiffpom", "mensweardog", "itsdougthepug", "marutaro",
+            "nala_cat", "juniperfoxx", "pumpkintheraccoon", "this_girl_is_a_squirrel", "iamlilbub", "reagandoodle",
+            "tunameltsmyheart", "loki", "maple.the.pup", "omame_munchkin", "tibbythecorgi", "toby_littledude",
+            "iggyjoey", "hamlet_the_piggy", "realdiddykong", "lionelthehog", "nevillejacobs", "jackthecockatiel"];
+
+        for (var i = 0; i < profiles[i].length; i++){
             $.getJSON("https://www.instagram.com/" + profiles[i] + "/?__a=1", function(resp) {
+                var tableData = [];
                 // Iterate over the JSON object
                 for (var j = 0, len = resp.graphql.user.edge_owner_to_timeline_media.edges.length; j < len; j++) {
                     tableData.push({
@@ -73,10 +107,11 @@
                     });
                 }    
                 table.appendRows(tableData);
-                doneCallback(); 
             });
         }
+        doneCallback(); 
     }
+    */
 
     tableau.registerConnector(myConnector);
 
