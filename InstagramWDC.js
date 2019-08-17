7cf7365fddd3598834c8a2e4bf6d82ca7b1ac86d
Yunoh <yunoh78@gmail.com>
2019-08-17 16:10:08 -0400

    Update InstagramWDC.js

--- InstagramWDC.js
+++ InstagramWDC.js
@@ -66,28 +66,6 @@
         }
     }
 
-    function getProfile(table, profile){
-        $.getJSON("https://www.instagram.com/" + profile + "/?__a=1", function(resp) {
-            var tableData = [];
-            // Iterate over the JSON object
-            for (var j = 0, len = resp.graphql.user.edge_owner_to_timeline_media.edges.length; j < len; j++) {
-                tableData.push({
-                    "graphql__user__biography": resp.graphql.user.biography,
-                    "graphql__user__edge_followed_by__count": resp.graphql.user.edge_followed_by.count,
-                    "graphql__user__full_name": resp.graphql.user.full_name,
-                    "graphql__user__is_business_account": resp.graphql.user.is_business_account,
-                    "graphql__user__is_verified": resp.graphql.user.is_verified,
-                    "graphql__user__username": resp.graphql.user.username,
-                    "graphql__user__edge_owner_to_timeline_media__count": resp.graphql.user.edge_owner_to_timeline_media.count,
-                    "graphql_user_edge_owner_to_timeline_media_edges_node_edge_media_to_comment_count": resp.graphql.user.edge_owner_to_timeline_media.edges[j].node.edge_media_to_comment.count,
-                    "graphql_user_edge_owner_to_timeline_media_edges_node_edge_liked_by_count": resp.graphql.user.edge_owner_to_timeline_media.edges[j].node.edge_liked_by.count
-                });
-            }    
-            table.appendRows(tableData);
-        });
-    }
-    
-
     /*
     myConnector.getData = function(table){
         var profiles = ["dorito.goldenboy", "bonebone29", "jiffpom", "mensweardog", "itsdougthepug", "marutaro",
@@ -128,3 +106,24 @@
     });
 
 })();
+
+function getProfile(table, profile){
+    $.getJSON("https://www.instagram.com/" + profile + "/?__a=1", function(resp) {
+        var tableData = [];
+        // Iterate over the JSON object
+        for (var j = 0, len = resp.graphql.user.edge_owner_to_timeline_media.edges.length; j < len; j++) {
+            tableData.push({
+                "graphql__user__biography": resp.graphql.user.biography,
+                "graphql__user__edge_followed_by__count": resp.graphql.user.edge_followed_by.count,
+                "graphql__user__full_name": resp.graphql.user.full_name,
+                "graphql__user__is_business_account": resp.graphql.user.is_business_account,
+                "graphql__user__is_verified": resp.graphql.user.is_verified,
+                "graphql__user__username": resp.graphql.user.username,
+                "graphql__user__edge_owner_to_timeline_media__count": resp.graphql.user.edge_owner_to_timeline_media.count,
+                "graphql_user_edge_owner_to_timeline_media_edges_node_edge_media_to_comment_count": resp.graphql.user.edge_owner_to_timeline_media.edges[j].node.edge_media_to_comment.count,
+                "graphql_user_edge_owner_to_timeline_media_edges_node_edge_liked_by_count": resp.graphql.user.edge_owner_to_timeline_media.edges[j].node.edge_liked_by.count
+            });
+        }    
+        table.appendRows(tableData);
+    });
+}
